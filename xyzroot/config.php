540a234001b3d1838d829bbc330200b3a65ce393
Cristian Adamo <cristian.adamo@gmail.com>
2011-08-01 01:36:45 -0300

    Revert "Revert a866b13f6713a90d70329c7b9d7801e0af84124a^..HEAD"
    This reverts commit 4b1bb6b52e5e173a4af674e536710b5a223860fa.

---
+++ xyzroot/config.php
@@ -0,0 +1,6 @@
+<?php
+// Copyright (c) 2011 Cristian Adamo. All rights reserved.
+// Use of this source code is governed by a Apache License (v2.0) that can be
+// found in the LICENSE file.
+
+return "http://doppler.example.com.ar";
